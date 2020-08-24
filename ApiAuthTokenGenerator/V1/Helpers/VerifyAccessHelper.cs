a612fa72999f1334a39dbd66b09f9609ff6985e9
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-08-24 14:03:49 +0100

    explicit test

--- ApiAuthTokenGenerator/V1/Helpers/VerifyAccessHelper.cs
+++ ApiAuthTokenGenerator/V1/Helpers/VerifyAccessHelper.cs
@@ -10,12 +10,13 @@ namespace ApiAuthTokenGenerator.V1.Helpers
     {
         public static bool ShouldHaveAccess(AuthorizerRequest authorizerRequest, AuthToken tokenData, string apiName)
         {
-            if (!tokenData.Enabled
+            if (tokenData.Enabled == false
                 || (tokenData.ExpirationDate != null && tokenData.ExpirationDate < DateTime.Now)
                 || tokenData.Environment != authorizerRequest.Environment
                 || tokenData.ApiEndpointName != authorizerRequest.ApiEndpointName)
             /* Redundant
             || tokenData.ApiName != apiName)*/
+
             {
                 LambdaLogger.Log($"Token with id {tokenData.Id} allowing access for {tokenData.ApiName} with endpoint {tokenData.ApiEndpointName}" +
                     $" in {tokenData.Environment} stage does not have access to {apiName} with endpoint {authorizerRequest.ApiEndpointName} " +
