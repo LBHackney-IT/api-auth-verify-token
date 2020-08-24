a76025865fcbfae1bfeb315262d04f00be6c889f
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-08-24 15:07:23 +0100

    implicit check

--- ApiAuthTokenGenerator/V1/Helpers/VerifyAccessHelper.cs
+++ ApiAuthTokenGenerator/V1/Helpers/VerifyAccessHelper.cs
@@ -10,7 +10,7 @@ namespace ApiAuthTokenGenerator.V1.Helpers
     {
         public static bool ShouldHaveAccess(AuthorizerRequest authorizerRequest, AuthToken tokenData, string apiName)
         {
-            if (tokenData.Enabled == false
+            if (!tokenData.Enabled
                 || (tokenData.ExpirationDate != null && tokenData.ExpirationDate < DateTime.Now)
                 || tokenData.Environment != authorizerRequest.Environment
                 || tokenData.ApiEndpointName != authorizerRequest.ApiEndpointName)
@@ -18,11 +18,16 @@ namespace ApiAuthTokenGenerator.V1.Helpers
             || tokenData.ApiName != apiName)*/
 
             {
-                LambdaLogger.Log($"Token with id {tokenData.Id} allowing access for {tokenData.ApiName} with endpoint {tokenData.ApiEndpointName}" +
-                    $" in {tokenData.Environment} stage does not have access to {apiName} with endpoint {authorizerRequest.ApiEndpointName} " +
-                    $"for {authorizerRequest.Environment} stage { tokenData.Enabled }");
+                LambdaLogger.Log($"Token with id {tokenData.Id} denying access for {tokenData.ApiName} with endpoint {tokenData.ApiEndpointName}" +
+                   $" in {tokenData.Environment} stage does not have access to {apiName} with endpoint {authorizerRequest.ApiEndpointName} " +
+                   $"for {authorizerRequest.Environment} stage { tokenData.Enabled }");
                 return false;
             }
+
+            LambdaLogger.Log($"Token with id {tokenData.Id} allowing access for {tokenData.ApiName} with endpoint {tokenData.ApiEndpointName}" +
+                   $" in {tokenData.Environment} stage does not have access to {apiName} with endpoint {authorizerRequest.ApiEndpointName} " +
+                   $"for {authorizerRequest.Environment} stage { tokenData.Enabled }");
+
             return true;
         }
     }
