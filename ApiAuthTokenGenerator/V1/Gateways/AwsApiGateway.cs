00a946ff1b79ffcdbb0b4d2af6b3e6d3f04e2d59
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2020-09-02 14:50:54 +0100

    changed logic for retrieving api name

--- ApiAuthTokenGenerator/V1/Gateways/AwsApiGateway.cs
+++ ApiAuthTokenGenerator/V1/Gateways/AwsApiGateway.cs
@@ -1,4 +1,6 @@
-using Amazon.APIGateway.Model;
+using Amazon.APIGateway;
+using Amazon.Runtime;
+
 using Amazon.Lambda.Core;
 using ApiAuthTokenGenerator.V1.Domain;
 using Newtonsoft.Json;
@@ -7,29 +9,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Amazon.APIGateway.Model;
 
 namespace ApiAuthTokenGenerator.V1.Gateways
 {
     public class AwsApiGateway : IAwsApiGateway
     {
-        private HttpClient _httpClient;
+        private AmazonAPIGatewayClient _client;
         public AwsApiGateway()
         {
-            _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri(@"https://apigateway.eu-west-2.amazonaws.com");
-
+            _client = new AmazonAPIGatewayClient();
         }
         public string GetApiName(string apiId)
         {
-            var response = _httpClient.GetAsync(new Uri($@"/restapis/{apiId}", UriKind.Relative)).Result;
+            var response = _client.GetRestApiAsync(new GetRestApiRequest { RestApiId = apiId }).Result;
+
             if (response == null)
             {
                 throw new AwsApiNotFoundException();
             }
-            var responseData = response.Content.ReadAsStringAsync().Result;
-
-            GetRestApiResponse apiResponse = JsonConvert.DeserializeObject<GetRestApiResponse>(responseData);
-            return apiResponse.Name;
+            _client.Dispose();
+            return response.Name;
         }
     }
 }
