9f2646f0f9e481aecf34fac3c789aa4bd48d012b
LBHMGeorgieva <mirela.georgieva@hackney.gov.uk>
2021-05-14 10:54:30 +0100

    add terraform to create dynamodb to staging and prod

---
+++ terraform/production/main.tf
@@ -0,0 +1,52 @@
+terraform {
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-2"
+}
+
+data "aws_caller_identity" "current" {}
+
+data "aws_region" "current" {}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-production-apis"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/api-authenticator/state"
+  }
+}
+
+
+resource "aws_dynamodb_table" "api_authenticator_dynamodb_table" {
+    name                  = "APIAuthenticatorData"
+    billing_mode          = "PROVISIONED"
+    read_capacity         = 10
+    write_capacity        = 10
+    hash_key              = "apiName"
+    range_key             = "environment"
+	
+    attribute {
+        name              = "apiName"
+        type              = "S"
+    }
+    attribute {
+        name              = "environment"
+        type              = "S"
+    }
+
+    tags = {
+        Name              = "api-authenticator-production"
+        Environment       = "production"
+        terraform-managed = true
+        project_name      = "api-authenticator"
+    }
+}
+
