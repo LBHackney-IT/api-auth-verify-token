2521c0890727e0fecbfad589928bbb956384d116
Callum Macpherson <callum.macpherson@hackney.gov.uk>
2025-03-18 11:36:26 +0000

    Refactor VerifyTokenHelper

--- ApiAuthVerifyToken/V1/Helpers/ValidateTokenHelper.cs
+++ ApiAuthVerifyToken/V1/Helpers/ValidateTokenHelper.cs
@@ -1,69 +1,37 @@
 using Amazon.Lambda.Core;
 using JWT;
 using JWT.Algorithms;
-using JWT.Builder;
 using JWT.Exceptions;
 using JWT.Serializers;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.IdentityModel.JsonWebTokens;
-using Microsoft.IdentityModel.Tokens;
-using Newtonsoft.Json.Linq;
-using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
-using System.Security.Cryptography;
-using System.Text;
-using System.Text.Encodings.Web;
 
 namespace ApiAuthVerifyToken.V1.Helpers
 {
     public static class ValidateTokenHelper
     {
+        private static readonly JwtDecoder _decoder;
 
-
-
-        public static List<Claim> ValidateTokenOld(string token, string secret)
+        static ValidateTokenHelper()
         {
-            try
-            {
-                JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            var algorithm = new HMACSHA256Algorithm();
+            var serializer = new JsonNetSerializer();
+            var validator = new JwtValidator(serializer, new UtcDateTimeProvider());
+            var urlEncoder = new JwtBase64UrlEncoder();
 
-                var handler = new JwtSecurityTokenHandler();
-                var validations = new TokenValidationParameters
-                {
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(secret)),
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    RequireExpirationTime = false,
-                    RequireSignedTokens = false,
-                };
-
-                var claims = handler.ValidateToken(token, validations, out var tokenSecure);
-                return claims.Claims.ToList();
-            }
-            catch (SecurityTokenInvalidSignatureException e)
-            {
-                LambdaLogger.Log($"Token beginning with {token.Substring(0, 8)} is not valid");
-                LambdaLogger.Log(e.ToString());
-                return null;
-            }
+            _decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
         }
 
-
-        private static string DecodeToken(string token, string secret)
+        public static List<Claim> ValidateToken(string token, string secret)
         {
             try
             {
-                var algorithm = new HMACSHA256Algorithm();
-                var serializer = new JsonNetSerializer();
-                var validator = new JwtValidator(serializer, new UtcDateTimeProvider());
-                var urlEncoder = new JwtBase64UrlEncoder();
-                var decoder = new JwtDecoder(serializer, validator, urlEncoder, algorithm);
-                return decoder.Decode(token, secret, verify: true);
+                var tokenJson = _decoder.Decode(token, secret, verify: true);
+                if (tokenJson == null) return null;
+
+                return ExtractClaims(token);
             }
             catch (SignatureVerificationException)
             {
@@ -72,12 +40,8 @@ namespace ApiAuthVerifyToken.V1.Helpers
             }
         }
 
-
-        public static List<Claim> ValidateToken(string token, string secret)
+        private static List<Claim> ExtractClaims(string token)
         {
-            var tokenJson = DecodeToken(token, secret);
-            if (tokenJson == null) return null;
-
             var handler = new JwtSecurityTokenHandler();
             var parsedToken = handler.ReadToken(token) as JwtSecurityToken;
             return parsedToken?.Claims.ToList() ?? new List<Claim>();
