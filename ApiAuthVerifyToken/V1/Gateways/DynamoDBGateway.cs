2ff2a7382794f6a1a4608bc95de10bf6f5164a54
Adam <74552077+adamtry@users.noreply.github.com>
2025-04-07 16:16:55 +0100

    Revert "Replace API Gateway SDK call with table lookup to prevent timeout (#81)"
    This reverts commit 9fbf33610979518b7e2404dedc6d30401c924e4e.

--- ApiAuthVerifyToken/V1/Gateways/DynamoDBGateway.cs
+++ ApiAuthVerifyToken/V1/Gateways/DynamoDBGateway.cs
@@ -14,40 +14,10 @@ namespace ApiAuthVerifyToken.V1.Gateways
     public class DynamoDBGateway : IDynamoDbGateway
     {
         private readonly IDynamoDBContext _dynamoDbContext;
-        const string ApiGatewayIdIndex = "apiGatewayIdIndex"; // Secondary index to query by ApiGatewayId
         public DynamoDBGateway(IDynamoDBContext dynamoDbContext)
         {
             _dynamoDbContext = dynamoDbContext;
         }
-
-        public APIDataUserFlow GetAPIDataByApiIdAsync(string apiAwsId)
-        {
-            try
-            {
-                var search = _dynamoDbContext.QueryAsync<APIDataUserFlowDbEntity>(
-                    apiAwsId,
-                    new DynamoDBOperationConfig
-                    {
-                        IndexName = ApiGatewayIdIndex,
-                    }
-                );
-
-                List<APIDataUserFlowDbEntity> results = search.GetRemainingAsync().Result;
-                if (results.Count == 0)
-                {
-                    LambdaLogger.Log($"API with id {apiAwsId} does not exist in DynamoDB");
-                    throw new APIEntryNotFoundException();
-                }
-                return results[0]?.ToDomain();
-
-            }
-            catch (Exception ex)
-            {
-                LambdaLogger.Log($"An error occurred retrieving data from DynamoDb while querying for {apiAwsId}. Message: {ex.Message}");
-                throw;
-            }
-        }
-
         public APIDataUserFlow GetAPIDataByNameAndEnvironmentAsync(string apiName, string environment)
         {
             try
