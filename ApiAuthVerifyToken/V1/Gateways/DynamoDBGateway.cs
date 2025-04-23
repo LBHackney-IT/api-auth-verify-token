93d912e427ac0661a1708ef6f324d2a05193bbe6
Adam <74552077+adamtry@users.noreply.github.com>
2025-04-23 11:12:50 +0100

    Revert "Release:  Replace API Gateway SDK call with DynamoDB lookup to prevent timeout"

--- ApiAuthVerifyToken/V1/Gateways/DynamoDBGateway.cs
+++ ApiAuthVerifyToken/V1/Gateways/DynamoDBGateway.cs
@@ -5,7 +5,9 @@ using ApiAuthVerifyToken.V1.Domain;
 using ApiAuthVerifyToken.V1.Factories;
 using ApiAuthVerifyToken.V1.Infrastructure;
 using System;
-using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ApiAuthVerifyToken.V1.Gateways
 {
@@ -16,36 +18,6 @@ namespace ApiAuthVerifyToken.V1.Gateways
         {
             _dynamoDbContext = dynamoDbContext;
         }
-
-        public APIDataUserFlow GetAPIDataByApiGatewayIdAsync(string apiGatewayId)
-        {
-            try
-            {
-                var table = _dynamoDbContext.GetTargetTable<APIDataUserFlowDbEntity>();
-                var search = table.Query(
-                    new QueryOperationConfig
-                    {
-                        IndexName = "apiGatewayIdIndex",
-                        Filter = new QueryFilter("apiGatewayId", QueryOperator.Equal, apiGatewayId)
-                    });
-
-                var documents = search.GetRemainingAsync().Result;
-                if (documents.Count == 0)
-                    throw new APIEntryNotFoundException($"API with id {apiGatewayId} does not exist in DynamoDB");
-                if (documents.Count > 1)
-                    LambdaLogger.Log($"WARNING: Multiple entries found for API with API Gateway ID {apiGatewayId} in DynamoDB: {documents.Select(d => d.ToJson())} - only the first will be returned");
-
-                var selectedDocument = documents.First();
-                var entity = _dynamoDbContext.FromDocument<APIDataUserFlowDbEntity>(selectedDocument);
-                return entity?.ToDomain();
-            }
-            catch (Exception ex)
-            {
-                LambdaLogger.Log($"An error occurred retrieving data from DynamoDb while querying for {apiGatewayId}. Message: {ex.Message}");
-                throw;
-            }
-        }
-
         public APIDataUserFlow GetAPIDataByNameAndEnvironmentAsync(string apiName, string environment)
         {
             try
