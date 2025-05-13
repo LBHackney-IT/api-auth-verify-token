cdc4ed82b47111d0951a11b0ea14fdb38d8e7321
LBHTKarki <tuomo.karki@hackney.gov.uk>
2025-05-13 09:08:49 +0100

    add mandatory tags

--- terraform/staging/main.tf
+++ terraform/staging/main.tf
@@ -1,14 +1,14 @@
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "~> 3.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-2"
+  region = "eu-west-2"
 }
 
 data "aws_caller_identity" "current" {}
@@ -26,36 +26,38 @@ terraform {
 
 
 resource "aws_dynamodb_table" "api_authenticator_dynamodb_table" {
-    name                  = "APIAuthenticatorData"
-    billing_mode          = "PAY_PER_REQUEST"
-    hash_key              = "apiName"
-    range_key             = "environment"
-	
-    attribute {
-        name              = "apiName"
-        type              = "S"
-    }
-    attribute {
-        name              = "environment"
-        type              = "S"
-    }
+  name         = "APIAuthenticatorData"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "apiName"
+  range_key    = "environment"
 
-    tags = {
-        Name              = "api-authenticator-staging"
-        Environment       = "staging"
-        terraform-managed = true
-        project_name      = "api-authenticator"
-    }
+  attribute {
+    name = "apiName"
+    type = "S"
+  }
+  attribute {
+    name = "environment"
+    type = "S"
+  }
 
-    global_secondary_index {
-        name            = "apiGatewayIdIndex"
-        hash_key        = "apiGatewayId"
-        projection_type = "ALL"
-    }
+  tags = {
+    Name              = "api-authenticator-staging"
+    Environment       = "stg"
+    terraform-managed = true
+    project_name      = "api-authenticator"
+    Application       = "API Authenticator"
+    TeamEmail         = "developementteam@hackney.gov.uk"
+  }
 
-    attribute {
-        name = "apiGatewayId"
-        type = "S"
-    }
+  global_secondary_index {
+    name            = "apiGatewayIdIndex"
+    hash_key        = "apiGatewayId"
+    projection_type = "ALL"
+  }
+
+  attribute {
+    name = "apiGatewayId"
+    type = "S"
+  }
 }
 
