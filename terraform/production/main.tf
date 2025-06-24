2d5c395ea743dcc6faa68f35ecd40d9d0df86197
Miles Alford <43789512+Miles-Alford@users.noreply.github.com>
2025-06-24 13:03:22 +0200

    Tear down DB and lambda

--- terraform/production/main.tf
+++ terraform/production/main.tf
@@ -25,41 +25,41 @@ terraform {
 }
 
 
-resource "aws_dynamodb_table" "api_authenticator_dynamodb_table" {
-  name         = "APIAuthenticatorData"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "apiName"
-  range_key    = "environment"
+# resource "aws_dynamodb_table" "api_authenticator_dynamodb_table" {
+#   name         = "APIAuthenticatorData"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "apiName"
+#   range_key    = "environment"
 
-  attribute {
-    name = "apiName"
-    type = "S"
-  }
-  attribute {
-    name = "environment"
-    type = "S"
-  }
+#   attribute {
+#     name = "apiName"
+#     type = "S"
+#   }
+#   attribute {
+#     name = "environment"
+#     type = "S"
+#   }
 
-  tags = {
-    Name              = "api-authenticator-production"
-    Environment       = "prod"
-    terraform-managed = true
-    project_name      = "api-authenticator"
-    Application       = "API Authenticator"
-    TeamEmail         = "developementteam@hackney.gov.uk"
-    BackupPolicy      = "Prod"
-    Confidentiality   = "Internal"
-  }
+#   tags = {
+#     Name              = "api-authenticator-production"
+#     Environment       = "prod"
+#     terraform-managed = true
+#     project_name      = "api-authenticator"
+#     Application       = "API Authenticator"
+#     TeamEmail         = "developementteam@hackney.gov.uk"
+#     BackupPolicy      = "Prod"
+#     Confidentiality   = "Internal"
+#   }
 
-  global_secondary_index {
-    name            = "apiGatewayIdIndex"
-    hash_key        = "apiGatewayId"
-    projection_type = "ALL"
-  }
+#   global_secondary_index {
+#     name            = "apiGatewayIdIndex"
+#     hash_key        = "apiGatewayId"
+#     projection_type = "ALL"
+#   }
 
-  attribute {
-    name = "apiGatewayId"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "apiGatewayId"
+#     type = "S"
+#   }
+# }
 
